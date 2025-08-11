0b02b771b6107be5ddeccfd83339f259f3624d76
Raúl Andrés Silva Velásquez <raul29247@gmail.com>
2025-08-11 12:42:54 -0500

    cors

--- src/main/java/ec/edu/espe/ms_api_gateway/MsApiGatewayApplication.java
+++ src/main/java/ec/edu/espe/ms_api_gateway/MsApiGatewayApplication.java
@@ -10,4 +10,17 @@ public class MsApiGatewayApplication {
 		SpringApplication.run(MsApiGatewayApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
+		return new org.springframework.web.servlet.config.annotation.WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
+
 }
